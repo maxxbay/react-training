3363216a5ac12ff79750661daffdfa56f8ae2c46
Maxxbay <maxxbay@gmail.com>
2022-12-14 13:39:07 +0100

    dual state props

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,26 +1,25 @@
 import './CostItem.css';
 import CostDate from './CostDate';
 import Card from '../UI/Card';
-import React, { useState } from 'react';
-import Button from './Button';
+// import Button from './Button';
 
 const CostItem = props => {
-  const [description, setDescription] = useState(props.description);
+  // const [description, setDescription] = useState(props.description);
 
   // console.log('Text in component definition');
 
-  const changeDescriptionHandler = () => {
-    setDescription('New cost-named item');
-  };
+  // const changeDescriptionHandler = () => {
+  //   setDescription('New cost-named item');
+  // };
 
   return (
     <Card className="cost-item">
       <CostDate date={props.date} />
       <div className="cost-item__description">
-        <h2>{description}</h2>
+        <h2>{props.description}</h2>
         <div className="cost-item__price">${props.amount}</div>
       </div>
-      <Button onClick={changeDescriptionHandler}>Change item</Button>
+      {/* <Button onClick={changeDescriptionHandler}>Change item</Button> */}
     </Card>
   );
 };
--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,31 +1,42 @@
+import React, { useState } from 'react';
 import CostItem from './CostItem';
 import './Costs.css';
 import Card from '../UI/Card';
+import CostsFilter from './CostsFilter';
 
 const Costs = props => {
+  const [selectedYear, setSelectedYear] = useState('2022');
+  const yearChangeHandler = year => {
+    setSelectedYear(year);
+  };
+
   return (
-    <Card className="costs">
-      <CostItem
-        date={props.costs[0].date}
-        description={props.costs[0].description}
-        amount={props.costs[0].amount}
-      />
-      <CostItem
-        date={props.costs[1].date}
-        description={props.costs[1].description}
-        amount={props.costs[1].amount}
-      />
-      <CostItem
-        date={props.costs[2].date}
-        description={props.costs[2].description}
-        amount={props.costs[2].amount}
-      />
-      <CostItem
-        date={props.costs[3].date}
-        description={props.costs[3].description}
-        amount={props.costs[3].amount}
-      />
-    </Card>
+    <div>
+      <Card className="costs">
+        <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
+
+        <CostItem
+          date={props.costs[0].date}
+          description={props.costs[0].description}
+          amount={props.costs[0].amount}
+        />
+        <CostItem
+          date={props.costs[1].date}
+          description={props.costs[1].description}
+          amount={props.costs[1].amount}
+        />
+        <CostItem
+          date={props.costs[2].date}
+          description={props.costs[2].description}
+          amount={props.costs[2].amount}
+        />
+        <CostItem
+          date={props.costs[3].date}
+          description={props.costs[3].description}
+          amount={props.costs[3].amount}
+        />
+      </Card>
+    </div>
   );
 };
 export default Costs;
